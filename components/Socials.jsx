0f3796df043507b087e37f2c2dadbab349608415
aditya07879 <aditya07879@gmail.com>
2026-02-01 08:03:47 +0530

    new tab open links

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -23,7 +23,8 @@ const Socials = ({ containerStyles, iconStyles }) => {
     <div className={containerStyles}>
       {socials.map((item, index) => {
         return (
-          <Link key={index} href={item.path} className={iconStyles}>
+          <Link key={index} href={item.path}  target="_blank"
+  rel="noopener noreferrer" className={iconStyles}>
             {item.icon}
           </Link>
         );
